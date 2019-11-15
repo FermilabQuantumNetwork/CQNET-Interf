b4895e2222dd4c7e690b8334bbf2e623bc14e11f
s1dav1s <s1dav1s@stanford.edu>
2019-11-14 19:17:59 -0800

    Plot stair data plus sine fit

--- 11.13to11.14run/plot_longstairs.py
+++ 11.13to11.14run/plot_longstairs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python2.7
 
 import numpy as np
-#import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import scipy.optimize
 #import tkinter
 #matplotlib.use('TkAgg')
 import datetime
@@ -11,10 +12,17 @@ import math
 import pymysql
 import os
 
-figname = "stairsNov13.png"
+mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname("/home/inqnet1/Desktop/CQNET/CQNET-Interf"))
 
-START_TIME = '2019-11-13 10:37:15'
-END_TIME = '2019-11-14 1:40:00'
+
+
+# #For 30min steps
+# START_TIME = '2019-11-13 10:37:15'
+# END_TIME = '2019-11-13 15:40:00'
+
+#For 60min steps
+START_TIME = '2019-11-13 17:39:26'
+END_TIME = '2019-11-14 5:50:34'
 
 
 
@@ -28,19 +36,14 @@ db = pymysql.connect(host="192.168.0.125",  # this PC
 					 db="teleportcommission",
 					 charset='utf8mb4',
 					 cursorclass=pymysql.cursors.DictCursor)
-#Initial visibility
-Vavmin = 0.3
-Vavmax0 =222.5
 
-print(Vavmin)
-print(Vavmax0)
-visibility = (Vavmax0 - Vavmin)/(Vavmax0 + Vavmin)
-print(visibility)
-print("")
+
+
+
 
 
 P = []
-time_P = []
+time_P =[]
 Vav=[]
 time_Vav=[]
 T1 = []
@@ -173,17 +176,47 @@ for t in time_T1:
 	time_T1_el_mins.append((elapsed.total_seconds())/60) #Convert elapsed time from seconds to minutes
 
 
-VavmaxArr=[]
-P0=P[0]
-for p in P:
-	VavmaxArr.append(Vavmax0*p/P0)
+print("Vmin: ", min(Vav))
+print("Vmax: ", max(Vav))
+Vmin=min(Vav)
+Vmax=max(Vav)
 
+
+
+
+minVav_index = np.argmin(Vav)
+PatVmin=P[minVav_index]
+maxVav_index = np.argmax(Vav)
+PatVmax=P[maxVav_index]
+
+Vavmin0=Vmin*P[0]/PatVmin
+Vavmax0=Vmax*P[0]/PatVmax
+
+VavmaxArr=[]
+VavminArr=[]
+#Check for noise level
+for p in P:
+	VavmaxArr.append(Vavmax0*p/PatVmin)
+	VavminArr.append(Vavmin0*p/P[0])
+
+VavmaxArr = np.array(VavmaxArr)
+VavminArr = np.array(VavminArr)
+
+visibility = (Vavmax0 - Vavmin0)/(Vavmax0 + Vavmin0)
+print("Visibility: ", visibility)
+print("")
+
+Vavmax=max(Vav)
+Vavmin=min(Vav)
 
 #convert to phase
 phase = []
 for n in range(len(Vav)):
 	v=Vav[n]
-	Vavmax=VavmaxArr[n]
+	#Vavmax=max(Vav)
+	#Vavmin=min(Vav)
+	#Vavmax=VavmaxArr[n]
+	#Vavmin=VavminArr[n]
 	ph = (2*v-Vavmax -Vavmin)/(Vavmax-Vavmin)
 	if ph >1 or ph<-1:
 		print("sinph: ",ph)
@@ -219,20 +252,25 @@ for n in range(len(Vav)):
 
 minPhase_indices = np.where(phase==min(phase))
 minPhase_indices=minPhase_indices[0]
-print(minPhase_indices[0])
+#print(minPhase_indices[0])
 maxPhase_indices=np.where(phase==max(phase))
 maxPhase_indices=maxPhase_indices[0]
-print("minphase vin: ",Vin[minPhase_indices[0]])
-print("minphase vap: ",Vap[minPhase_indices[0]])
-print("maxphase vin: ",Vin[maxPhase_indices[0]])
-print("maxphase vap: ",Vap[maxPhase_indices[0]])
+#print("minphase vin: ",Vin[minPhase_indices[0]])
+#print("minphase vap: ",Vap[minPhase_indices[0]])
+#print("maxphase vin: ",Vin[maxPhase_indices[0]])
+#print("maxphase vap: ",Vap[maxPhase_indices[0]])
+print("")
+
+
+
 
 
 #Stacked plot of all data
 fig, axs = plt.subplots(6,1, num=238, sharex=True)
-xmin=time_Vap_el_mins[0] #40
-xmax=time_Vap_el_mins[-1] #60
-#Vin
+xmin=time_Vap_el_mins[0]
+xmax=time_Vap_el_mins[-1]
+
+#Vap
 axs[0].plot(time_Vap_el_mins, Vap,  linestyle = 'none', marker = '.', markersize = 2)
 axs[0].set_ylabel(r"$V_{ap}$ (V)")
 axs[0].grid()
@@ -246,13 +284,12 @@ axs[1].grid()
 axs[2].plot(time_Vav_el_mins, Vav,  linestyle = 'none', marker = '.', markersize = 2)
 axs[2].set_ylabel(r"Vav (mV)")
 axs[2].grid()
-#axs[2].set_ylim(210,220)
+
 
 #Phase
 axs[3].plot(time_Vav_el_mins, phase,  linestyle = 'none', marker = '.', markersize = 2)
 axs[3].set_ylabel(r"Phase ($\degree$)")
 axs[3].grid()
-#axs[3].set_ylim(60,80)
 
 #Temp
 axs[4].plot(time_T1_el_mins, T1,  linestyle = 'none', marker = '.', markersize = 2)
@@ -270,6 +307,530 @@ xmin1=xlims[0]
 xmax1=xlims[1]
 fig.suptitle("Monitoring Interferometer from \n"+str(time_Vav[0]+datetime.timedelta(minutes=xmin1))+" to "+str(time_Vav[0]+datetime.timedelta(minutes=xmax1)))
 plt.xlabel('Elapsed time (min)', fontsize =16)
-plt.savefig(figname)
 plt.show()
-print(figname)
+
+
+
+
+meanPhiArr = []
+rangePhiArr = []
+meanVapArr=[]
+meanVinArr = []
+meanTArr=[]
+
+
+#phi = -2
+t1 = 260
+t2 = 300
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+#phi = -32
+t1 = 200
+t2 = 240
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+#phi = -56
+t1=20
+t2=60
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+#phi = -60
+t1=144
+t2=184
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+
+
+#phi = -85
+t1=80
+t2=120
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+#phi = 25
+t1=325
+t2=326
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+#phi = 26
+t1=690
+t2=730
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+#phi = 46
+t1=624
+t2=664
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+#phi = 67
+t1=445
+t2=485
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+
+
+#phi = 68
+t1=565
+t2=605
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+#phi = 85
+t1=509
+t2=549
+ind1 = np.where(np.array(time_Vav_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vav_el_mins)==t2)
+ind2=int(ind2[0])
+print(phase[ind1])
+print(phase[ind2])
+meanphi = np.mean(phase[ind1:ind2])
+rangephi = max(phase[ind1:ind2]) - min(phase[ind1:ind2])
+print("mean phi: ", meanphi)
+print("range phi: ", rangephi)
+meanPhiArr.append(meanphi)
+rangePhiArr.append(rangephi)
+
+ind1 = np.where(np.array(time_Vap_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vap_el_mins)==t2)
+ind2=int(ind2[0])
+meanVap = np.mean(Vap[ind1:ind2])
+meanVapArr.append(meanVap)
+
+ind1 = np.where(np.array(time_Vin_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_Vin_el_mins)==t2)
+ind2=int(ind2[0])
+meanVin = np.mean(Vin[ind1:ind2])
+meanVinArr.append(meanVin)
+
+ind1 = np.where(np.array(time_T1_el_mins)==t1)
+ind1=int(ind1[0])
+ind2 = np.where(np.array(time_T1_el_mins)==t2)
+ind2=int(ind2[0])
+meanT = np.mean(T1[ind1:ind2])
+meanTArr.append(meanT)
+print("")
+
+print(meanPhiArr)
+print(rangePhiArr)
+print(meanVapArr)
+print(meanVinArr)
+print(meanTArr)
+
+
+
+
+
+#Fit to sine curve
+def fit_sin(tt, yy):
+	'''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
+	tt = np.array(tt)
+	yy = np.array(yy)
+	ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+	Fyy = abs(np.fft.fft(yy))
+	guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+	guess_amp = np.std(yy) * 2.**0.5
+	guess_offset = np.mean(yy)
+	guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+	def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
+	popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+	A, w, p, c = popt
+	print(A, w, p,c)
+	f = w/(2.*np.pi)
+	fitfunc = lambda t: A * np.sin(w*t + p) + c
+	return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
+
+meanVapArr=np.array(meanVapArr)
+meanVinArr=np.array(meanVinArr)
+meanTArr=np.array(meanTArr)
+
+fineVapArr= np.linspace(min(meanVapArr), max(meanVapArr), 100)
+resVap=fit_sin(meanVapArr, meanPhiArr)
+VapfitArr = resVap["fitfunc"](fineVapArr)
+
+fineVinArr= np.linspace(min(meanVinArr), max(meanVinArr), 100)
+resVin=fit_sin(meanVinArr, meanPhiArr)
+VinfitArr = resVap["fitfunc"](fineVinArr)
+
+fineTArr= np.linspace(min(meanTArr), max(meanTArr), 100)
+resT=fit_sin(meanTArr, meanPhiArr)
+TfitArr = resT["fitfunc"](fineTArr)
+
+maxVapFit = max(meanVapArr)
+minVapFit=min(meanVapArr)
+visib = (maxVapFit-minVapFit)/(maxVapFit+minVapFit)
+
+print(visib)
+
+#Stacked plot of all data
+fig, axs = plt.subplots(3,1, num=258)
+
+#Phi vs Vap
+phimaxfit = max(VapfitArr)
+phiminfit = min(VapfitArr)
+
+axs[0].errorbar(meanVapArr, meanPhiArr, yerr=np.std(meanPhiArr), linestyle = 'none', marker = '.', markersize = 4)
+axs[0].plot(fineVapArr,VapfitArr, '-r')
+axs[0].set_ylabel(r"Mean $\phi (\degree)$")
+axs[0].set_xlabel(r"Mean $V_{ap}$ (V)")
+axs[0].set_title("Phase vs. Voltage Sent to Power Supply "+ r"($V_{ap}$)")
+axs[0].grid()
+
+#Phi vs Vin
+axs[1].errorbar(meanVinArr, meanPhiArr, yerr=np.std(meanPhiArr), linestyle = 'none', marker = '.', markersize = 4)
+axs[1].plot(fineVinArr,VinfitArr, '-r')
+axs[1].set_ylabel(r"Mean $\phi$ ($\degree$)")
+axs[1].set_xlabel(r"Mean $V_{in}$ (V)")
+axs[1].set_title("Phase vs. Actual Voltage of Power Supply "+ r"($V_{ap}$)")
+axs[1].grid()
+
+#Phi vs T
+axs[2].errorbar(meanTArr, meanPhiArr, yerr=np.std(meanPhiArr), linestyle = 'none', marker = '.', markersize = 4)
+axs[2].plot(fineTArr,TfitArr, '-r')
+axs[2].set_ylabel(r"Mean $\phi$ ($\degree$)")
+axs[2].set_xlabel(r"Mean T ($\degree$C)")
+axs[2].set_title("Phase vs. Temp")
+axs[2].grid()
+
+plt.subplots_adjust(hspace = 0.6)
+
+#fig.suptitle(r"$\phi_{min}$"+" = {:.1f}".format(min(meanPhiArr)) + r", $\phi_{max}$"+" = {:.1f}".format(max(meanPhiArr)))
+#fig.suptitle(r"$\phi_{min}$"+" = {:.1f}".format(phiminfit) + r", $\phi_{max}$"+" = {:.1f}".format(phimaxfit))
+fig.suptitle("Fit Visibility: {}".format(visib))
+plt.show()
